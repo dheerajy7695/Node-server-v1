8290998ee85e74f83fac4531648e953b247ae5a8
dheerajy7695 <dheerajy906@gmail.com>
2025-02-15 23:38:48 +0530

    Added cloud db storage

--- api/core/config/dbSource.js
+++ api/core/config/dbSource.js
@@ -1,4 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const DbPwd = process.env.DB_PASSWORD;
+
 module.exports = {
-    // dbUrl: 'mongodb+srv://testAdmin:Node_pwd65@dheeraj-mongodb-sofjk.mongodb.net/test?authSource=admin',
-    dbUrl :'mongodb+srv://testAdmin:Node_pwd65@dheeraj-mongodb.sofjk.mongodb.net/test'
+    dbUrl: 'mongodb+srv://testAdmin:Node_pwd65@dheeraj-mongodb.sofjk.mongodb.net/test',
+    newDbUrl: `mongodb+srv://javascript142:${DbPwd}@cluster0.uvxhe.mongodb.net/`
 }
